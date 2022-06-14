19d516ee3ce21f22c53e53c0beb8726d23a2a57c
Ryli Davis <rylidavis94@gmail.com>
2022-06-13 17:08:32 -0700

    Other Job Role Chosen and Displaying Text Field

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-
+//Default focus on name field when page first loads
 window.onload = function () {
     document.getElementById("name").focus();
 }
 
+//Hiding other job role text field by default
 const other = document.getElementById('other-job-role');
 other.style.display = 'none';
+
+
+
+/*Function that uses conditional to check if user chooses 'other job role'. If so,
+the default hidden display for 'other job role' text field will be blocked and appear to the user. */
+function displayotherjobRole() {
+    const jobOption = document.getElementById('title').value;
+    if (jobOption == 'other') {
+        other.style.display = 'block';
+    } else {
+        other.style.display = 'none';
+    }
+
+}
+
+
+
+
 
 
 
